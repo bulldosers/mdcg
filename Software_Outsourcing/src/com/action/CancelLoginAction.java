6f7ab45c3075ccfa160f0627bb27443e2db59a62
fuwanzeng <625983683@qq.com>
2016-11-26 14:03:47 +0800

    modifyLogin

--- Software_Outsourcing/src/com/action/CancelLoginAction.java
+++ Software_Outsourcing/src/com/action/CancelLoginAction.java
@@ -13,37 +13,40 @@ import com.Entity.User;
 import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.ActionSupport;
 
-public class CancelLoginAction extends ActionSupport{
-	private static final long serialVersionUID = 8013816027944871760L;  
-    private User user;
+public class CancelLoginAction extends ActionSupport {
+	private static final long serialVersionUID = 8013816027944871760L;
+	private User user;
+
 	public User getUser() {
 		return user;
 	}
+
 	public void setUser(User user) {
 		this.user = user;
 	}
-    @Override
-    public String execute() throws UnsupportedEncodingException{
-    	Map session = ActionContext.getContext().getSession();
-    	HttpServletRequest req = ServletActionContext.getRequest();
-    	String nowLogin =  (String) session.get("NowLogining");
-    	System.out.println("µ±Ç°ÓÃ»§ÊÇ"+nowLogin);
-    	if(nowLogin.length()>0){
-	     Cookie cs[] = req.getCookies();
-	    	if(cs!=null){
-	    	for(Cookie c:cs){
-			String name = URLDecoder.decode(c.getName(),"UTF-8");
-     		String value = URLDecoder.decode(c.getValue(),"UTF-8");
-    		if(name.equals(nowLogin)){
-    			   Cookie cookie = new Cookie(c.getName(),c.getValue());
-    		        c.setMaxAge(0);
-    			    req.removeAttribute("NowLogining");
-    	                return SUCCESS;
-	    	}
-		   }
-	    	}
-    	}
-		req.setAttribute("login_error", "µ±Ç°Ã»ÓÐÓÃ»§µÇÂ¼");
-			return "error";
-    }
+
+	@Override
+	public String execute() throws UnsupportedEncodingException {
+		Map session = ActionContext.getContext().getSession();
+		HttpServletRequest req = ServletActionContext.getRequest();
+		String nowLogin = (String) session.get("NowLogining");
+		System.out.println("ï¿½ï¿½Ç°ï¿½Ã»ï¿½ï¿½ï¿½" + nowLogin);
+		if (nowLogin.length() > 0) {
+			Cookie cs[] = req.getCookies();
+			if (cs != null) {
+				for (Cookie c : cs) {
+					String name = URLDecoder.decode(c.getName(), "UTF-8");
+					String value = URLDecoder.decode(c.getValue(), "UTF-8");
+					if (name.equals(nowLogin)) {
+						Cookie cookie = new Cookie(c.getName(), c.getValue());
+						c.setMaxAge(0);
+						req.removeAttribute("NowLogining");
+						return SUCCESS;
+					}
+				}
+			}
+		}
+		req.setAttribute("login_error", "ï¿½ï¿½Ç°Ã»ï¿½ï¿½ï¿½Ã»ï¿½ï¿½ï¿½Â¼");
+		return "error";
+	}
 }
--- Software_Outsourcing/src/com/dao/UserDAO.java
+++ Software_Outsourcing/src/com/dao/UserDAO.java
@@ -5,17 +5,17 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 
-import com.Entity.Info;  
-import com.Entity.Project; 
+import com.Entity.Info;
+import com.Entity.Project;
 
 public class UserDAO {
 	private static UserDAO userDao = null;
 	private static final String INFO_TABLE_NAME = "info";
-	private static final String LINK_TABLE_NAME = "link"; 
-	private static final String PROJECT_TABLE_NAME = "project"; 
+	private static final String LINK_TABLE_NAME = "link";
+	private static final String PROJECT_TABLE_NAME = "project";
 	private static final String INVITATION_TABLE_NAME = "invitation";
 	private Statement stmt;
-	
+
 	public static UserDAO getUserDAO() {
 		if (userDao == null) {
 			userDao = new UserDAO();
@@ -27,266 +27,251 @@ public class UserDAO {
 		}
 		return userDao;
 	}
-	
+
 	private static Info ORM(ResultSet res) throws SQLException {
-		return new Info(res.getString("name"),res.getString("sex"),res.getString("age"), 
-				res.getString("major"),res.getString("phone"), res.getString("school"),
-				res.getString("introduce"),res.getString("username") );
+		return new Info(res.getString("name"), res.getString("sex"), res.getString("age"), res.getString("major"),
+				res.getString("phone"), res.getString("school"), res.getString("introduce"), res.getString("username"));
 	}
-	
-	public void Invite(String projname,String username) throws SQLException{
+
+	public void Invite(String projname, String username) throws SQLException {
 		stmt = JdbcUtils.getStatement();
-		String sql = "INSERT INTO " + INVITATION_TABLE_NAME + 
-				"(proname,username)" + " values('"
-				+ projname + "','"
+		String sql = "INSERT INTO " + INVITATION_TABLE_NAME + "(proname,username)" + " values('" + projname + "','"
 				+ username + "')";
 		System.out.println("邀请用户的SQL语句：" + sql);
 		stmt.executeUpdate(sql);
 	}
-	
+
 	public Info getUserByUsername(String username) throws SQLException {
 		stmt = JdbcUtils.getStatement();
-		String sql = "SELECT * FROM " + INFO_TABLE_NAME + " WHERE username = '"
-				+ username + "'";
+		String sql = "SELECT * FROM " + INFO_TABLE_NAME + " WHERE username = '" + username + "'";
 		ResultSet res = stmt.executeQuery(sql);
 		if (res.next())
 			return ORM(res);
-		System.out.println("username=" + username +" not find!");
+		System.out.println("username=" + username + " not find!");
 		return null;
 	}
-	
-	public void addInfo(Info newInfo,String username) throws SQLException {
+
+	public void addInfo(Info newInfo, String username) throws SQLException {
 		stmt = JdbcUtils.getStatement();
-		String sql = "INSERT INTO " + INFO_TABLE_NAME + 
-				"(name,sex,age,major,phone,school,username,introduce)" + " values('"
-				+ newInfo.getName() + "','"
-				+ newInfo.getAge() + "','"
-				+ newInfo.getSex() + "','"
-				+ newInfo.getMajor() + "','"
-				+ newInfo.getPhone() + "','"
-				+ newInfo.getSchool() + "','" 
-				+ username + "','" 
-				+ newInfo.getIntroduce() + "')";
+		String sql = "INSERT INTO " + INFO_TABLE_NAME + "(name,sex,age,major,phone,school,username,introduce)"
+				+ " values('" + newInfo.getName() + "','" + newInfo.getAge() + "','" + newInfo.getSex() + "','"
+				+ newInfo.getMajor() + "','" + newInfo.getPhone() + "','" + newInfo.getSchool() + "','" + username
+				+ "','" + newInfo.getIntroduce() + "')";
 		System.out.println("注册用户的SQL语句：" + sql);
 		stmt.executeUpdate(sql);
 	}
-	
-	public void updateInfo(Info newInfo,String username) throws SQLException {
+
+	public void updateInfo(Info newInfo, String username) throws SQLException {
 		stmt = JdbcUtils.getStatement();
-		String sql = "UPDATE " + INFO_TABLE_NAME + " SET " + "name = '"
-				+ newInfo.getName() + "'," + "age = '"
-				+ newInfo.getAge() + "'," + "sex = '"
-				+ newInfo.getSex() + "'," + "major = '"
-				+ newInfo.getMajor() + "'," + "school = '"
-				+ newInfo.getSchool() + "'," + "phone = '"
-				+ newInfo.getPhone() + "'," + "introduce = '" 
-				+ newInfo.getIntroduce() + "'" +
-				" where username='" + username + "'";
+		String sql = "UPDATE " + INFO_TABLE_NAME + " SET " + "name = '" + newInfo.getName() + "'," + "age = '"
+				+ newInfo.getAge() + "'," + "sex = '" + newInfo.getSex() + "'," + "major = '" + newInfo.getMajor()
+				+ "'," + "school = '" + newInfo.getSchool() + "'," + "phone = '" + newInfo.getPhone() + "',"
+				+ "introduce = '" + newInfo.getIntroduce() + "'" + " where username='" + username + "'";
 		System.out.println("更新SQL语句:" + sql);
 		stmt.executeUpdate(sql);
 	}
-	
+
 	public Project ReadProj(ResultSet res) throws SQLException {
-		return new Project(res.getString("name"),res.getInt("lim"),res.getString("phone"), 
-				res.getString("major"),res.getString("school"), res.getInt("num"),
-				res.getString("context"),res.getString("req") ,res.getString("username"));
+		return new Project(res.getString("name"), res.getInt("lim"), res.getString("phone"), res.getString("major"),
+				res.getString("school"), res.getInt("num"), res.getString("context"), res.getString("req"),
+				res.getString("username"));
 	}
-	
-	public Project getProjByName(String name,String username) throws SQLException{
+
+	public Project getProjByName(String name, String username) throws SQLException {
 		stmt = JdbcUtils.getStatement();
-		String sql = "SELECT * FROM " + PROJECT_TABLE_NAME + " where name='" + name +"' && username='" + username +"'"; 
-		System.out.println("查询项目的sql语句:"+sql);
-		ResultSet res = stmt.executeQuery(sql); 
-		if(res.next()) return ReadProj(res);
+		String sql = "SELECT * FROM " + PROJECT_TABLE_NAME + " where name='" + name + "' && username='" + username
+				+ "'";
+		System.out.println("查询项目的sql语句:" + sql);
+		ResultSet res = stmt.executeQuery(sql);
+		if (res.next())
+			return ReadProj(res);
 		return null;
 	}
-	
-	public Project getProjByNameCT(String name,String context) throws SQLException{
+
+	public Project getProjByNameCT(String name, String context) throws SQLException {
 		stmt = JdbcUtils.getStatement();
-		String sql = "SELECT * FROM " + PROJECT_TABLE_NAME + " where name='" + name +"' && context='" + context +"'"; 
-		System.out.println("查询项目的sql语句:"+sql);
-		ResultSet res = stmt.executeQuery(sql); 
-		if(res.next()) return ReadProj(res);
+		String sql = "SELECT * FROM " + PROJECT_TABLE_NAME + " where name='" + name + "' && context='" + context + "'";
+		System.out.println("查询项目的sql语句:" + sql);
+		ResultSet res = stmt.executeQuery(sql);
+		if (res.next())
+			return ReadProj(res);
 		return null;
 	}
-	
-	public ArrayList<Project> SearchProsByName(String name) throws SQLException{
+
+	public ArrayList<Project> SearchProsByName(String name) throws SQLException {
 		stmt = JdbcUtils.getStatement();
-		String sql = "SELECT * FROM " + PROJECT_TABLE_NAME + " where name like '%" + name +"%'"; 
-		System.out.println("搜索项目的sql语句:"+sql);
-		ResultSet res = stmt.executeQuery(sql); 
+		String sql = "SELECT * FROM " + PROJECT_TABLE_NAME + " where name like '%" + name + "%'";
+		System.out.println("搜索项目的sql语句:" + sql);
+		ResultSet res = stmt.executeQuery(sql);
 		ArrayList<Project> pros = new ArrayList<Project>();
-		while (res.next()) { Project pro = ReadProj(res); pros.add(pro); } 
+		while (res.next()) {
+			Project pro = ReadProj(res);
+			pros.add(pro);
+		}
 		return pros;
 	}
-	
-	public ArrayList<Project> getPros(String username) throws SQLException{ 
+
+	public ArrayList<Project> getPros(String username) throws SQLException {
 		stmt = JdbcUtils.getStatement();
-		String sql = "SELECT * FROM " + PROJECT_TABLE_NAME + " where username='" + username +"'"; 
-		System.out.println("获得我的项目的sql语句:"+sql);
-		ResultSet res = stmt.executeQuery(sql);  
+		String sql = "SELECT * FROM " + PROJECT_TABLE_NAME + " where username='" + username + "'";
+		System.out.println("获得我的项目的sql语句:" + sql);
+		ResultSet res = stmt.executeQuery(sql);
 		ArrayList<Project> pros = new ArrayList<Project>();
-		while (res.next()) { Project pro = ReadProj(res); pros.add(pro); } 
+		while (res.next()) {
+			Project pro = ReadProj(res);
+			pros.add(pro);
+		}
 		return pros;
 	}
-	
-	public ArrayList<Project> getAllPros() throws SQLException{ 
+
+	public ArrayList<Project> getAllPros() throws SQLException {
 		stmt = JdbcUtils.getStatement();
-		String sql = "SELECT * FROM " + PROJECT_TABLE_NAME; 
-		System.out.println("获得我的项目的sql语句:"+sql);
-		ResultSet res = stmt.executeQuery(sql);  
+		String sql = "SELECT * FROM " + PROJECT_TABLE_NAME;
+		System.out.println("获得我的项目的sql语句:" + sql);
+		ResultSet res = stmt.executeQuery(sql);
 		ArrayList<Project> pros = new ArrayList<Project>();
-		while (res.next()) { Project pro = ReadProj(res); pros.add(pro); } 
+		while (res.next()) {
+			Project pro = ReadProj(res);
+			pros.add(pro);
+		}
 		return pros;
 	}
-	
-	public void AddProj(Project pro,String username) throws SQLException{
+
+	public void AddProj(Project pro, String username) throws SQLException {
 		stmt = JdbcUtils.getStatement();
-		String sql = "INSERT INTO " + PROJECT_TABLE_NAME + 
-				"(name,lim,phone,major,school,num,context,req,username)" + " values('"
-				+ pro.getName() + "','"
-				+ pro.getLim() + "','"
-				+ pro.getPhone() + "','"
-				+ pro.getMajor() + "','"
-				+ pro.getSchool() + "','"
-				+ pro.getNum() + "','"
-				+ pro.getContext() + "','" 
-				+ pro.getReq() + "','" 
-				+ username + "')";
+		String sql = "INSERT INTO " + PROJECT_TABLE_NAME + "(name,lim,phone,major,school,num,context,req,username)"
+				+ " values('" + pro.getName() + "','" + pro.getLim() + "','" + pro.getPhone() + "','" + pro.getMajor()
+				+ "','" + pro.getSchool() + "','" + pro.getNum() + "','" + pro.getContext() + "','" + pro.getReq()
+				+ "','" + username + "')";
 		System.out.println("添加用户的SQL语句：" + sql);
 		stmt.executeUpdate(sql);
-//		sql = "INSERT INTO " + LINK_TABLE_NAME + 
-//				"(project,username)" + " values('"
-//				+ pro.getName() + "','" 
-//				+ username + "')";
-//		System.out.println("注册用户的SQL语句：" + sql);
-//		stmt.executeUpdate(sql);
+		// sql = "INSERT INTO " + LINK_TABLE_NAME +
+		// "(project,username)" + " values('"
+		// + pro.getName() + "','"
+		// + username + "')";
+		// System.out.println("注册用户的SQL语句：" + sql);
+		// stmt.executeUpdate(sql);
 	}
-	
-	public void UpdateProj(Project pro,String username) throws SQLException{
+
+	public void UpdateProj(Project pro, String username) throws SQLException {
 		stmt = JdbcUtils.getStatement();
-		String sql = "UPDATE " + PROJECT_TABLE_NAME + " SET " +   
-				"lim='" + pro.getLim() + "'," +
-				"phone='" + pro.getPhone() + "'," +
-				"major='" + pro.getMajor() + "'," +
-				"school='" + pro.getSchool() + "'," +
-				"num='" + pro.getNum() + "'," +
-				"context='" + pro.getContext() + "'," +
-				"req='" + pro.getReq() + "'"
+		String sql = "UPDATE " + PROJECT_TABLE_NAME + " SET " + "lim='" + pro.getLim() + "'," + "phone='"
+				+ pro.getPhone() + "'," + "major='" + pro.getMajor() + "'," + "school='" + pro.getSchool() + "',"
+				+ "num='" + pro.getNum() + "'," + "context='" + pro.getContext() + "'," + "req='" + pro.getReq() + "'"
 				+ " where username='" + username + "' && name='" + pro.getName() + "'";
 		System.out.println("更新项目的SQL语句：" + sql);
-		stmt.executeUpdate(sql); 
-	}
-	
-	public void DelProj(String name,String username) throws SQLException{
-		stmt = JdbcUtils.getStatement();
-		String sql = "DELETE FROM " + PROJECT_TABLE_NAME + 
-				" WHERE name='" + name + "' && username='" + username + "'"; 
-		System.out.println("删除项目的sql语句："+sql);
-		int i = stmt.executeUpdate(sql);
-		System.out.println("ret " + i); 
-	}
-	
-	public void DelInvitation(String projname,String username) throws SQLException{
-		stmt = JdbcUtils.getStatement();
-		String sql = "DELETE FROM " + INVITATION_TABLE_NAME + 
-				" WHERE proname='" + projname + "' && username='" + username + "'"; 
-		System.out.println("删除邀请的sql语句："+sql);
-		int i = stmt.executeUpdate(sql);
-		System.out.println("ret " + i); 
-	}
-	
-	public void RemoveJoiner(String name,String username) throws SQLException{
-		stmt = JdbcUtils.getStatement();
-		String sql = "DELETE FROM " + LINK_TABLE_NAME + 
-				" WHERE proname='" + name + "' && username='" + username + "'"; 
-		System.out.println("删除项目的sql语句："+sql);
-		int i = stmt.executeUpdate(sql);
-		System.out.println("ret " + i); 
-	}
-	
-	public void ApplyProj(String projname,String username) throws SQLException{
-		stmt = JdbcUtils.getStatement();
-		String sql = "INSERT INTO " + LINK_TABLE_NAME + 
-				"(proname,username)" + " values('"
-				+ projname + "','"
-				+ username + "')"; 
 		stmt.executeUpdate(sql);
-		System.out.println("添加关联的sql语句："+sql);
 	}
-	
-	public void DelTask(String projname,String username) throws SQLException{
+
+	public void DelProj(String name, String username) throws SQLException {
 		stmt = JdbcUtils.getStatement();
-		String sql = "DELETE FROM " + LINK_TABLE_NAME + 
-				" WHERE proname='" + projname + "' && username='" + username + "'"; 
-		System.out.println("删除项目的sql语句："+sql);
+		String sql = "DELETE FROM " + PROJECT_TABLE_NAME + " WHERE name='" + name + "' && username='" + username + "'";
+		System.out.println("删除项目的sql语句：" + sql);
 		int i = stmt.executeUpdate(sql);
 		System.out.println("ret " + i);
 	}
-	
-	public ArrayList<Info> AllJoinersByProjname(String projname) throws SQLException{
+
+	public void DelInvitation(String projname, String username) throws SQLException {
 		stmt = JdbcUtils.getStatement();
-		String sql = "SELECT * FROM " + LINK_TABLE_NAME + " where proname='" + projname +"'"; 
-		System.out.println("搜索用户名称的sql语句:"+sql);
-		ResultSet res = stmt.executeQuery(sql); 
+		String sql = "DELETE FROM " + INVITATION_TABLE_NAME + " WHERE proname='" + projname + "' && username='"
+				+ username + "'";
+		System.out.println("删除邀请的sql语句：" + sql);
+		int i = stmt.executeUpdate(sql);
+		System.out.println("ret " + i);
+	}
+
+	public void RemoveJoiner(String name, String username) throws SQLException {
+		stmt = JdbcUtils.getStatement();
+		String sql = "DELETE FROM " + LINK_TABLE_NAME + " WHERE proname='" + name + "' && username='" + username + "'";
+		System.out.println("删除项目的sql语句：" + sql);
+		int i = stmt.executeUpdate(sql);
+		System.out.println("ret " + i);
+	}
+
+	public void ApplyProj(String projname, String username) throws SQLException {
+		stmt = JdbcUtils.getStatement();
+		String sql = "INSERT INTO " + LINK_TABLE_NAME + "(proname,username)" + " values('" + projname + "','" + username
+				+ "')";
+		stmt.executeUpdate(sql);
+		System.out.println("添加关联的sql语句：" + sql);
+	}
+
+	public void DelTask(String projname, String username) throws SQLException {
+		stmt = JdbcUtils.getStatement();
+		String sql = "DELETE FROM " + LINK_TABLE_NAME + " WHERE proname='" + projname + "' && username='" + username
+				+ "'";
+		System.out.println("删除项目的sql语句：" + sql);
+		int i = stmt.executeUpdate(sql);
+		System.out.println("ret " + i);
+	}
+
+	public ArrayList<Info> AllJoinersByProjname(String projname) throws SQLException {
+		stmt = JdbcUtils.getStatement();
+		String sql = "SELECT * FROM " + LINK_TABLE_NAME + " where proname='" + projname + "'";
+		System.out.println("搜索用户名称的sql语句:" + sql);
+		ResultSet res = stmt.executeQuery(sql);
 		ArrayList<Info> joiners = new ArrayList<Info>();
 		ArrayList<String> names = new ArrayList<String>();
-		while (res.next()) {names.add(res.getString("username")); }
-		for(String username :names)
-		{
-			System.out.println("用户名称："+ username);
+		while (res.next()) {
+			names.add(res.getString("username"));
+		}
+		for (String username : names) {
+			System.out.println("用户名称：" + username);
 			Info tmp = UserDAO.getUserDAO().getUserByUsername(username);
-			if(tmp!=null) joiners.add(tmp);  
+			if (tmp != null)
+				joiners.add(tmp);
 		}
 		return joiners;
 	}
-	
-	public ArrayList<Info> AllJoiners() throws SQLException{
-		stmt = JdbcUtils.getStatement(); 
-		String sql = "SELECT * FROM " + INFO_TABLE_NAME; 
-		System.out.println("搜索用户名称的sql语句:"+sql);
-		ResultSet res = stmt.executeQuery(sql); 
+
+	public ArrayList<Info> AllJoiners() throws SQLException {
+		stmt = JdbcUtils.getStatement();
+		String sql = "SELECT * FROM " + INFO_TABLE_NAME;
+		System.out.println("搜索用户名称的sql语句:" + sql);
+		ResultSet res = stmt.executeQuery(sql);
 		ArrayList<Info> joiners = new ArrayList<Info>();
-		while (res.next()) {joiners.add(ORM(res));} 
+		while (res.next()) {
+			joiners.add(ORM(res));
+		}
 		return joiners;
 	}
-	
-	public ArrayList<Project> MyTasks(String username) throws SQLException{
+
+	public ArrayList<Project> MyTasks(String username) throws SQLException {
 		stmt = JdbcUtils.getStatement();
-		String sql = "SELECT * FROM " + LINK_TABLE_NAME + " where username='" + username +"'"; 
-		System.out.println("搜索项目名称的sql语句:"+sql);
-		ResultSet res = stmt.executeQuery(sql); 
+		String sql = "SELECT * FROM " + LINK_TABLE_NAME + " where username='" + username + "'";
+		System.out.println("搜索项目名称的sql语句:" + sql);
+		ResultSet res = stmt.executeQuery(sql);
 		ArrayList<Project> pros = new ArrayList<Project>();
 		ArrayList<String> names = new ArrayList<String>();
-		while (res.next()) {names.add(res.getString("proname")); }
-		for(String projname :names)
-		{
-			System.out.println("任务名称："+ projname);
-			ArrayList<Project> tmp = SearchProsByName(projname );
-			for(Project proj : tmp)
-			{
-				pros.add(proj); 
+		while (res.next()) {
+			names.add(res.getString("proname"));
+		}
+		for (String projname : names) {
+			System.out.println("任务名称：" + projname);
+			ArrayList<Project> tmp = SearchProsByName(projname);
+			for (Project proj : tmp) {
+				pros.add(proj);
 			}
 		}
 		return pros;
 	}
-	
-	public ArrayList<Project> MyInvitations(String username) throws SQLException{
+
+	public ArrayList<Project> MyInvitations(String username) throws SQLException {
 		stmt = JdbcUtils.getStatement();
-		String sql = "SELECT * FROM " + INVITATION_TABLE_NAME + " where username='" + username +"'";
-		System.out.println("搜索邀请名称的sql语句:"+sql);
-		ResultSet res = stmt.executeQuery(sql); 
+		String sql = "SELECT * FROM " + INVITATION_TABLE_NAME + " where username='" + username + "'";
+		System.out.println("搜索邀请名称的sql语句:" + sql);
+		ResultSet res = stmt.executeQuery(sql);
 		ArrayList<String> names = new ArrayList<String>();
-		while (res.next()) {names.add(res.getString("proname")); }
-		 
-		ArrayList<Project> pros = new ArrayList<Project>(); 
-		for(String projname :names)
-		{
-			System.out.println("任务名称："+ projname);
-			ArrayList<Project> tmp = SearchProsByName(projname );
-			for(Project proj : tmp)
-			{
-				pros.add(proj); 
+		while (res.next()) {
+			names.add(res.getString("proname"));
+		}
+
+		ArrayList<Project> pros = new ArrayList<Project>();
+		for (String projname : names) {
+			System.out.println("任务名称：" + projname);
+			ArrayList<Project> tmp = SearchProsByName(projname);
+			for (Project proj : tmp) {
+				pros.add(proj);
 			}
 		}
 		return pros;
